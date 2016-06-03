c0d07dcff9f0bd6ab57a786d10175374daac25ed
yurykozhenov <yurykozhenov@gmail.com>
2016-06-03 11:50:21 +0300

    Fix models bug with initializing

--- models.py
+++ models.py
@@ -190,7 +190,7 @@ class Mark(BaseModel):
 
     subject = ForeignKeyField(Subject, related_name='marks')
     student = ForeignKeyField(Student, related_name='marks')
-    mark = IntegerField(constraints=[Check('grade > 0 AND grade < 6')])
+    mark = IntegerField(constraints=[Check('mark > 0 AND mark < 6')])
 
     @classmethod
     def create_random_mark(cls, n_subjects=10, n_students=10):
@@ -217,9 +217,9 @@ def initialize():
         DATABASE.create_tables([Student, Teacher, Subject, Mark])
         fill_db(40, 20, 30, 200)
         print("Database was successfully initialized!")
-
     except OperationalError:
         print("Database already initialized!")
+
 
     DATABASE.close()
 
